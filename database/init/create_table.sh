bc370a8335d5fa240e1b90672ff6f09ae5e6793d
b1017034 <b1017034@gmail.com>
2023-05-08 02:22:14 +0900

    WIP

--- database/init/create_table.sh
+++ database/init/create_table.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-#CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
+CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
+$CMD_MYSQL -e "create table todo (
+    id int(10) AUTO_INCREMENT NOT NULL primary key,
+    text varchar(1000) NOT NULL,
+    due_date datetime NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    );"
+$CMD_MYSQL -e  "insert into todo (text, due_date) values ('テストのメモ1', cast('2023-05-09 24:00:00' as datetime));"
